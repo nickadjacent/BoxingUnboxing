9ee7b0a374610c23780aae1d7a5b8430e2f11996
Nick_Ram <60027609+nickadjacent@users.noreply.github.com>
2020-04-08 11:54:10 -0700

    Boxing/Unboxing

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Box some string data into a variable
+            List<object> list = new List<object>();
+            list.Add(7);
+            list.Add(28);
+            list.Add(-1);
+            list.Add(true);
+            list.Add("chair");
+            //Make sure it is the type you need before proceeding
+            if (list is object)
+            {
+                //This shouldn't run
+                Console.WriteLine("I guess we have an integer value in this box?");
+            }
+            if (list is string)
+            {
+                Console.WriteLine("It is totally a string in the box!");
+            }
+
+        }
+    }
+}
