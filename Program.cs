992a2445eef06d7520704c6f58c040fdf4e1b120
Nick_Ram <60027609+nickadjacent@users.noreply.github.com>
2020-04-08 13:22:37 -0700

    Update

--- Program.cs
+++ Program.cs
@@ -8,24 +8,31 @@ namespace BoxingUnboxing
     {
         static void Main(string[] args)
         {
-            //Box some string data into a variable
             List<object> list = new List<object>();
             list.Add(7);
             list.Add(28);
             list.Add(-1);
             list.Add(true);
             list.Add("chair");
-            //Make sure it is the type you need before proceeding
-            if (list is object)
+            for (int i = 0; i < list.Count; i++)
             {
-                //This shouldn't run
-                Console.WriteLine("I guess we have an integer value in this box?");
+                Console.WriteLine(list[i]);
             }
-            if (list is string)
+            int sum = 0;
+            for (int i = 0; i < list.Count; i++)
             {
-                Console.WriteLine("It is totally a string in the box!");
+                if (list[i] is int)
+                {
+                    sum += (int)list[i];
+                }
             }
+            Console.WriteLine(sum);
 
+
+            // object ActuallyString = "a string";
+            // string ExplicitString = ActuallyString as string;
+
+            // Console.WriteLine(ExplicitString);
         }
     }
 }
